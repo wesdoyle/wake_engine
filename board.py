fa1e90204124197668c026629d63072e345074fa
wesdoyle <wesdoyle08@gmail.com>
2020-12-06 16:49:34 -0600

    Add bitshift to mask knight moves

--- board.py
+++ board.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from constants import FileSquares as fsq, RankSquares as rsq
+
 class Board():
 
     """
@@ -18,19 +20,38 @@ class Board():
     def __init__(self):
         self.reset_bb()
 
-        self.white_R_bb = self.create_empty_bitmap()
-        self.white_K_bb = self.create_empty_bitmap()
-        self.white_B_bb = self.create_empty_bitmap()
-        self.white_P_bb = self.create_empty_bitmap()
-        self.white_N_bb = self.create_empty_bitmap()
-        self.white_Q_bb = self.create_empty_bitmap()
+        self.rank_1_bb = self.make_empty_bitmap()
+        self.rank_2_bb = self.make_empty_bitmap()
+        self.rank_3_bb = self.make_empty_bitmap()
+        self.rank_4_bb = self.make_empty_bitmap()
+        self.rank_5_bb = self.make_empty_bitmap()
+        self.rank_6_bb = self.make_empty_bitmap()
+        self.rank_7_bb = self.make_empty_bitmap()
+        self.rank_8_bb = self.make_empty_bitmap()
+        self.file_a_bb = self.make_empty_bitmap()
+        self.file_b_bb = self.make_empty_bitmap()
+        self.file_c_bb = self.make_empty_bitmap()
+        self.file_d_bb = self.make_empty_bitmap()
+        self.file_e_bb = self.make_empty_bitmap()
+        self.file_f_bb = self.make_empty_bitmap()
+        self.file_g_bb = self.make_empty_bitmap()
+        self.file_h_bb = self.make_empty_bitmap()
 
-        self.black_R_bb = self.create_empty_bitmap()
-        self.black_K_bb = self.create_empty_bitmap()
-        self.black_B_bb = self.create_empty_bitmap()
-        self.black_P_bb = self.create_empty_bitmap()
-        self.black_N_bb = self.create_empty_bitmap()
-        self.black_Q_bb = self.create_empty_bitmap()
+        self.make_rank_file_bitmaps()
+
+        self.white_R_bb = self.make_empty_bitmap()
+        self.white_K_bb = self.make_empty_bitmap()
+        self.white_B_bb = self.make_empty_bitmap()
+        self.white_P_bb = self.make_empty_bitmap()
+        self.white_N_bb = self.make_empty_bitmap()
+        self.white_Q_bb = self.make_empty_bitmap()
+
+        self.black_R_bb = self.make_empty_bitmap()
+        self.black_K_bb = self.make_empty_bitmap()
+        self.black_B_bb = self.make_empty_bitmap()
+        self.black_P_bb = self.make_empty_bitmap()
+        self.black_N_bb = self.make_empty_bitmap()
+        self.black_Q_bb = self.make_empty_bitmap()
 
         self.init_pieces()
 
@@ -54,16 +75,14 @@ class Board():
         self._set_black()
 
     def reset_bb(self):
-        self.bb = self.create_empty_bitmap()
+        self.bb = self.make_empty_bitmap()
 
-    # slide east
     def plus1(self, square):
         for i in range(square, 64, 1):
             self.bb[i] = 1
             if not (i+1) % 8:
                 return
 
-    # slide north west
     def plus7(self, square):
         for i in range(square, 64, 7):
             self.bb[i] = 1
@@ -82,8 +101,52 @@ class Board():
             if not (i+1) % 8:
                 return
 
-    def get_rank_attack(self, rank):
-        self.rank_attacks = np.ndarray(64, 256)
+    def minus1(self, square):
+        for i in range(square, 0, -1):
+            self.bb[i] = 1
+            if not (i) % 8:
+                return
+
+    def minus7(self, square):
+        for i in range(square, 0, -7):
+            self.bb[i] = 1
+            if not (i+1) % 8:
+                return
+
+    def minus8(self, square):
+        for i in range(square, 0, -8):
+            self.bb[i] = 1
+            if not (i+1) % 8:
+                return
+
+    def minus9(self, square):
+        for i in range(square, -1, -9):
+            self.bb[i] = 1
+            if not (i) % 8:
+                return
+
+    def knight_attacks(self, square):
+        mask = self.make_empty_bitmap()
+        if square in fsq.a:
+            mask = np.bitwise_or(self.file_g_bb, self.file_h_bb)
+        elif square in fsq.b:
+            mask = self.file_g_bb
+        elif square in fsq.g:
+            mask = self.file_a_bb
+        elif square in fsq.h:
+            mask = np.bitwise_or(self.file_a_bb, self.file_b_bb)
+        if square in rsq._1:
+            mask = np.bitwise_or(self.file_g_bb, self.file_h_bb)
+        attacks = self.make_empty_bitmap()
+        for i in [0, 6, 15, 17, 10, -6, -15, -17, -10]:
+            if square + abs(i) > 64:
+                # skip OOB
+                continue
+            attacks[square + i] = 1
+        if 1 in mask:
+            # bit shift the attacks by mask
+            attacks = attacks >> mask
+        return attacks
 
     def update_occupied_squares_bb(self):
         result = np.zeros(64, "byte")
@@ -94,8 +157,29 @@ class Board():
     def get_empty_squares_bb(self):
         return  1 - self.occupied_squares_bb
 
-    def create_empty_bitmap(self):
+    def make_empty_bitmap(self):
         return np.zeros(64, dtype="byte")
+
+    def make_rank_file_bitmaps(self):
+        # todo: faster numpy methods
+        for val in fsq.a:
+            self.file_a_bb[val] = 1
+        for val in fsq.b: self.file_b_bb[val] = 1
+        for val in fsq.c: self.file_c_bb[val] = 1
+        for val in fsq.d: self.file_d_bb[val] = 1
+        for val in fsq.e: self.file_e_bb[val] = 1
+        for val in fsq.f: self.file_f_bb[val] = 1
+        for val in fsq.g: self.file_g_bb[val] = 1
+        for val in fsq.h: self.file_h_bb[val] = 1
+
+        for val in rsq._1: self.rank_1_bb[val] = 1
+        for val in rsq._2: self.rank_2_bb[val] = 1
+        for val in rsq._3: self.rank_3_bb[val] = 1
+        for val in rsq._4: self.rank_4_bb[val] = 1
+        for val in rsq._5: self.rank_5_bb[val] = 1
+        for val in rsq._6: self.rank_6_bb[val] = 1
+        for val in rsq._7: self.rank_7_bb[val] = 1
+        for val in rsq._8: self.rank_8_bb[val] = 1
 
     def _set_white(self):
         for i in range(8, 16):
