07d8e0743febe1766efcd2022e77362a19f12f9c
wesdoyle <wesdoyle08@gmail.com>
2020-12-13 12:06:01 -0600

    Organize

--- temp.py
+++
@@ -1,8 +0,0 @@
-def most_significant_1_bit_table(bit: np.uint64) -> np.uint64:
-    if bit > np.uint64(127): return np.uint64(7)
-    if bit > np.uint64(63):  return np.uint64(6)
-    if bit > np.uint64(31):  return np.uint64(5)
-    if bit > np.uint64(15):  return np.uint64(4)
-    if bit > np.uint64(7):   return np.uint64(3)
-    if bit > np.uint64(1):   return np.uint64(1)
-    return np.uint64(0)
